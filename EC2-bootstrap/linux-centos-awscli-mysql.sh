0f65d6559a1ff198c6e9a325b7b6eb0ead0890dd
kum_cp <kum_cp@yahoo.com.vn>
2021-08-23 00:40:33 +0700

    add mysql install code

---
+++ EC2-bootstrap/linux-centos-awscli-mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+cd /var/www/html
+echo "<html>" > index.html
+
+
+wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+
+md5sum mysql57-community-release-el7-9.noarch.rpm
+
+sudo rpm -ivh mysql57-community-release-el7-9.noarch.rpm
+
+sudo yum install mysql-server -y
+
+sudo systemctl start mysqld
+
