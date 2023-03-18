623fbb31482d666a19d4cf0a42b39b322af1ed9f
hungran <26101787+hungran@users.noreply.github.com>
2023-03-19 00:04:59 +0700

    typo

--- EC2-bootstrap/ubuntu20-awscli.sh
+++ EC2-bootstrap/ubuntu20-awscli.sh
@@ -23,6 +23,7 @@ then
 else
     export TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
     export METADATA='-s -H "X-aws-ec2-metadata-token: '$TOKEN'" http://169.254.169.254'    
+fi
 echo "<br>Private IP: " >> index.html
 curl $METADATA/meta-data/local-ipv4 >> index.html
 
