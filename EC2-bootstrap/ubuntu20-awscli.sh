bb4d39e74c4359d480e760581d10e4e77a8590b5
hungran <26101787+hungran@users.noreply.github.com>
2023-03-18 23:54:25 +0700

    fix: Amazon Linux 2023 force to use IMDSv2

--- EC2-bootstrap/ubuntu20-awscli.sh
+++ EC2-bootstrap/ubuntu20-awscli.sh
@@ -16,12 +16,19 @@ echo "<html>" > index.html
 
 echo "<h1>Welcome to CodeStar</h1>" >> index.html
 echo "<h4>You are running instance from this IP (This is for testing purpose only, you should not public this to user):</h4>"
-
+status_code=$(curl -s -o /dev/null -w "%{http_code}" http://169.254.169.254/latest/meta-data/)
+if [[ "$status_code" -eq 200 ]]
+then
+    export METADATA='http://169.254.169.254'
+else
+    export TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+    export METADATA='-s -H "X-aws-ec2-metadata-token: '$TOKEN'" http://169.254.169.254'    
 echo "<br>Private IP: " >> index.html
-curl http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+curl $METADATA/meta-data/local-ipv4 >> index.html
 
 echo "<br>Public IP: " >> index.html
-curl http://169.254.169.254/latest/meta-data/public-ipv4 >> index.html 
+curl $METADATA/latest/meta-data/public-ipv4 >> index.html 
+echo "</html>" >> index.html 
 
 echo "</html>" >> index.html
 
