b2a237953e27a7378dbe6ff7e4e2b851902a2495
hungran <26101787+hungran@users.noreply.github.com>
2023-03-20 11:00:07 +0700

    Update linux-centos-awscli.sh

--- EC2-bootstrap/linux-centos-awscli.sh
+++ EC2-bootstrap/linux-centos-awscli.sh
@@ -27,6 +27,3 @@ curl $METADATA/latest/meta-data/local-ipv4 >> index.html
 echo "<br>Public IP: " >> index.html
 curl $METADATA/latest/meta-data/public-ipv4 >> index.html 
 echo "</html>" >> index.html
-
-echo "</html>" >> index.html
-
