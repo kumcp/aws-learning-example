5cbfecde13df81ff2068846030bda9372de9cd46
kum_cp <kum_cp@yahoo.com.vn>
2023-04-23 20:37:54 +0700

    update script for codedeployagent

---
+++ EC2-bootstrap/ubuntu22-docker-codedeployagent.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo chmod 666 /var/run/docker.sock
+
+
+sudo apt update
+
+sudo apt install ruby-full -y
+
+
+wget https://aws-codedeploy-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/latest/install
+
+chmod +x ./install
+
+sudo ./install auto
