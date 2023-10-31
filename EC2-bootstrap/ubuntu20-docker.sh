f3f0f19072162a40ceb80bb49e4a150eaf16a5f3
Kum <kum_cp@yahoo.com.vn>
2023-10-31 20:13:54 +0700

    Update ubuntu20-docker.sh
    Add auto yes

--- EC2-bootstrap/ubuntu20-docker.sh
+++ EC2-bootstrap/ubuntu20-docker.sh
@@ -6,7 +6,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
