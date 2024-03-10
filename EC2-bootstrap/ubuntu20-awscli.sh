c610dbb0fb2fbe384e3c0d14302233476616615e
Kum <kum_cp@yahoo.com.vn>
2024-03-10 20:45:26 +0700

    Update ubuntu20-awscli.sh

--- EC2-bootstrap/ubuntu20-awscli.sh
+++ EC2-bootstrap/ubuntu20-awscli.sh
@@ -21,7 +21,7 @@ if [[ "$status_code" -eq 200 ]]
 then
     export METADATA='http://169.254.169.254'
 else
-    export TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+    export TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
     export METADATA='-s -H "X-aws-ec2-metadata-token: '$TOKEN'" http://169.254.169.254'    
 fi
 echo "<br>Private IP: " >> index.html
